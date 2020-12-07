331e523e6b6a21d9e1a49e4ddef9f36c311d7ab1
Taennan Rickman <Taennan@imac.local>
2020-12-07 12:51:58 +1200

    Initial Commit

---
+++ Unnamed Space Game/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Unnamed Space Game
+//
+//  Created by Taennan Rickman on 7/12/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
